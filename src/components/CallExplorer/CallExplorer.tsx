70cc46d5b1778054c40864c388dd1bc3543decbc
Bart Tyrpien <bartlomein123@gmail.com>
2024-11-29 12:33:15 -0500

    css cleanup

--- src/components/CallExplorer/CallExplorer.tsx
+++ src/components/CallExplorer/CallExplorer.tsx
@@ -2,13 +2,17 @@
 import React, { useState } from "react";
 import { TransactionSearch } from "../TransactionSearch/TransactionSearch";
 import { InvocationFlow } from "../InvocationFlow/InvocationFlow";
+import { Title } from "../Title/Title";
 
 const CallExplorer = () => {
   const [txHash, setTxHash] = useState("");
 
   return (
     <div className="w-full h-screen">
-      <div className="max-w-6xl mx-auto mx-auto flex justify-end ">
+      <div className="max-w-6xl mx-auto mx-auto flex justify-between">
+        <div className="pt-4">
+          <Title align="left" name="Transaction Explorer" size="base" />
+        </div>
         <div className="w-[400px] p-4">
           <TransactionSearch onChange={setTxHash} />
         </div>
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 type Align = "left" | "right" | "center";
+type Size = "sm" | "base" | "lg" | "xl" | "2xl" | "3xl";
 
-export const Title = ({ name, align }: { name: string; align: Align }) => {
+export const Title = ({
+  name,
+  align,
+  size = "lg",
+}: {
+  name: string;
+  align: Align;
+  size?: Size;
+}) => {
   return (
     <div className={`text-${align}`}>
-      <span className="text-white-800 font-mono font-bold text-lg">{name}</span>
+      <span className={`text-white-800 font-mono font-bold text-${size} `}>
+        {name}
+      </span>
     </div>
   );
 };
